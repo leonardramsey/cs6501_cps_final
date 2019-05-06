07d4b0d89e188d577aa48d02e8e860d76e7cda3f
Leonard Ramsey <lr3hj@virginia.edu>
2019-05-05 21:12:04 -0400

    Cleaned up code

--- z3_project.py
+++ z3_project.py
@@ -52,24 +52,6 @@ def speech_gen(prev_speech, volume, pitch, speed, vol_min, vol_max, pitch_min, p
               pitch_d_min <= abs(pitch[0] - prev_speech[1]), abs(pitch[0] - prev_speech[1]) <= pitch_d_max,
               speed_d_min <= abs(speed[0] - prev_speech[2]), abs(speed[0] - prev_speech[2]) <= speed_d_max)
 
-        # # for increase vol...
-        # s.add(prev_speech[0] <= volume[0])
-        #
-        # # for decrease vol...
-        # s.add(prev_speech[0] >= volume[0])
-        #
-        # # for increase pitch...
-        # s.add(prev_speech[1] <= pitch[0])
-        #
-        # # for decrease pitch...
-        # s.add(prev_speech[1] >= pitch[0])
-        #
-        # # for increase speed
-        # s.add(prev_speech[2] <= speed[0])
-        #
-        # # for decrease speed
-        # s.add(prev_speech[2] >= speed[0])
-
 def coverage_consistence(C_l, C_g):
     if C_l[0] < C_g[0]: # vol_min
         return False
@@ -87,16 +69,10 @@ def coverage_consistence(C_l, C_g):
         return True
 
 
-# Each array is a 2D array
-# e.g. solArrayVol = [[1, 2, 3], [4, 5, 6]] means the road network has 2 segments and each segment has 3 waypoints.
-#      The X-coordinates of the 3 waypoints of Segment1 are 1, 2, and 3, the X-coordinates of the 3 waypoints of Segment2 are 4, 5, and 6.
-# For your outputs, the size of your solArrayVol/Y/Z will be (solN * N), which means your road network has solN segments, and each segment has N waypoints
-# number of curves..."N" from Q1
 solArrayVol = []
 solArrayPitch = []
 solArraySpeed = []
 curNumArray = []
-
 
 def main():
     # Coverage consistency check
@@ -136,27 +112,6 @@ def main():
         solArraySpeed.append([model[speed[0]].as_long()])
 
         curNumArray.append(N)
-        # s.reset()
-        # s.add(solArrayVol[sol_ind][N - 1] == volume[0], solArrayPitch[sol_ind][N - 1] == pitch[0],
-        #       solArraySpeed[sol_ind][N - 1] == speed[0])
-        # s.add(
-        #     abs(((pitch[0] - solArrayPitch[sol_ind][N - 2])) / ((volume[0] - solArrayVol[sol_ind][N - 2])) -
-        #
-        #         ((pitch[1] - solArrayPitch[sol_ind][N - 1])) / (volume[1] - solArrayVol[sol_ind][N - 1])) >= C_g[2],
-        #
-        #     abs(((pitch[0] - solArrayPitch[sol_ind][N - 2])) / (volume[0] - solArrayVol[sol_ind][N - 2]) -
-        #
-        #         ((pitch[1] - solArrayPitch[sol_ind][N - 1])) / (volume[1] - solArrayVol[sol_ind][N - 1])) <= C_g[3],
-        #
-        #     abs(((speed[0] - solArraySpeed[sol_ind][N - 2])) / (volume[0] - solArrayVol[sol_ind][N - 2]) -
-        #
-        #         (((speed[1] - solArraySpeed[sol_ind][N - 1])) / (volume[1] - solArrayVol[sol_ind][N - 1]))) >= C_g[2],
-        #
-        #     abs(((speed[0] - solArraySpeed[sol_ind][N - 2])) / (volume[0] - solArrayVol[sol_ind][N - 2]) -
-        #
-        #         (speed[1] - solArraySpeed[sol_ind][N - 1]) / (volume[1] - solArrayVol[sol_ind][N - 1])) <= C_g[3]
-        #
-        #     )
 
     print('final solArrayVol')
     print(solArrayVol)
@@ -164,21 +119,14 @@ def main():
     print(solArrayPitch)
     print('final solArraySpeed')
     print(solArraySpeed)
-    # Write the generated road information into a file
+    
+    # Write the generated speech information into a file
     file = open("speech.txt", "w")
-    # for seg in range(N):
     file.write('Speech ' + str(0 + 1) + ':\n')
-        # for point in range(curNumArray[seg]):
     file.write(
         str(solArrayVol[0][0]) + ',' + str(solArrayPitch[0][0]) + ',' + str(solArraySpeed[0][0]) + "\n")
     file.close()
 
-
-    # DO NOT CHANGE THIS PART OF THE CODE
-    # Additional part for generating the correct output format for visualising the generated road in Unity
-
-    # if you wish to view the generated coordinates without alterations for Unity, just comment out
-    # this last part up to if __name__ == '__main__':
     with open('speech.txt') as f:
         content = f.readlines()
 
